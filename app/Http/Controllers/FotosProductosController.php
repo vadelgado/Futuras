c2ba84cabf1976474917d1e1cd6f271c2f35c7fe
vadelgado <vadelgado@udenar.edu.co>
2024-07-25 08:43:23 -0500

    Refactor import statement for FotosProductosController.php

--- app/Http/Controllers/FotosProductosController.php
+++ app/Http/Controllers/FotosProductosController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\FotosProductos;
-use App\Models\Productos;
+use App\Models\productos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
