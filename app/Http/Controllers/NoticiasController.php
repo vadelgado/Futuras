070f62b2bb739eb99f4ee7037a3fd4165634e36a
vadelgado <vadelgado@udenar.edu.co>
2024-07-26 18:17:02 -0500

    feat: Refactor NoticiasController to order news articles by creation date

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -14,8 +14,7 @@ class NoticiasController extends Controller
      */
     public function index()
     {
-        $noticias = Noticias::all()
-        ->orderBy('created_at', 'desc');
+        $noticias = Noticias::orderBy('created_at', 'desc')->get();
         return Inertia::render('Noticias/Index', [
             'noticias' => $noticias
         ]);
